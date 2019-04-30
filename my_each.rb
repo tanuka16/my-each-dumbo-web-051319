ba3b597a654894abb41c941ac714abcbebf0ece6
Tanuka Das <das.tanuka@yahoo.com>
2019-04-30 23:49:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -7,3 +7,27 @@ def my_each(array) # put argument(s) here
   end
   array
 end
+
+=begin
+def my_map
+  result = []
+  each do |item|
+    result << yield(item)
+  end
+  result
+end
+
+def my_inject(initial=nil)
+  acc = initial
+  my_each do |item|
+    if acc.nil?
+      acc = item
+    else
+      acc = yield(acc, item)
+    end
+  end
+  acc
+end
+
+end
+=end
